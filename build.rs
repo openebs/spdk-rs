c72ee831647b4199e3ea1d5087003eeb3cc4200d
Xinliang Liu <xinliang.liu@linaro.org>
2022-06-15 10:49:44 +0000

    fix(style): make cfg line align with code
    Signed-off-by: Xinliang Liu <xinliang.liu@linaro.org>

--- build.rs
+++ build.rs
@@ -135,7 +135,7 @@ fn configure_spdk() -> Result<LibraryConfig, Error> {
         "spdk_accel",
         "spdk_accel_ioat",
         "spdk_bdev_aio",
-    #[cfg(target_arch = "x86_64")]
+        #[cfg(target_arch = "x86_64")]
         "spdk_bdev_crypto",
         "spdk_bdev_delay",
         "spdk_bdev_error",
