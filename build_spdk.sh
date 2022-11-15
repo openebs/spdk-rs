7a054be1fb1557884c54fb1da2f4f9f905dac889
Tiago Castro <tiagolobocastro@gmail.com>
2022-11-15 11:00:29 +0000

    fix: get the correct location for fio include files (#19)
    This was broken after splitting fio into 2 output derivations.
    
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>

--- build_spdk.sh
+++ build_spdk.sh
@@ -1,4 +1,4 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 # This script replicates the build of SPDK as its done by nix without setting
 # any specifics in terms of CPU. The purpose is to easily make changes to libspdk
 # locally and then recompile it and test it with mayastor.
@@ -14,7 +14,7 @@ pushd spdk || { echo "Can not find spdk directory"; exit; }
 	--with-uring \
 	--disable-unit-tests \
 	--disable-tests \
-	--with-fio=$(which fio | sed s';bin/fio;include;')
+	--with-fio=$(echo $NIX_CFLAGS_COMPILE | grep -o '/nix/store/[^ ]*fio[^ ]*-dev\/include' | head -n 1)
 
 make -j $(nproc)
 
