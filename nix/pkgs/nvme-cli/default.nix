4bbf9fc2dbaad666a0ef85a2499c36b3b14a842b
Tiago Castro <tiagolobocastro@gmail.com>
2024-10-30 18:15:06 +0000

    build: relax warning flags for nvme-cli
    Newest nixpkgs adds more strict build flags.
    Ideally we should just not build our own, but for now let's make it
    build as is.
    
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>

--- nix/pkgs/nvme-cli/default.nix
+++ nix/pkgs/nvme-cli/default.nix
@@ -9,6 +9,8 @@ stdenv.mkDerivation rec {
   buildInputs = [ libuuid ];
 
   makeFlags = [ "DESTDIR=$(out)" "PREFIX=" ];
+  # Required to build with latest nixpkgs with adds more warning/error
+  NIX_CFLAGS_COMPILE = [ "-Wno-enum-int-mismatch" "-Wno-array-bounds" "-Wno-use-after-free" ];
 
   # To omit the hostnqn and hostid files that are impure and should be unique
   # for each target host:
