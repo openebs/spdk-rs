df94281404a4af4f7903136655340959c5b1f1d4
Alexander Yanovskyy <alexander.yanovs-kyy@etu.unistra.fr>
2025-04-10 09:02:06 +0200

    trying to get hello world from spdk code base working

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -1,29 +1,92 @@
-use std::{
-    ffi::CString,
-    mem::zeroed,
-    os::raw::{c_char, c_int, c_void},
-};
+use std::{ffi::CString, mem::zeroed, os::raw::c_char};
 
 use spdk_rs::libspdk::{
-    size_t, spdk_app_fini, spdk_app_opts, spdk_app_opts_init, spdk_app_parse_args, spdk_app_start,
-    spdk_app_stop, SPDK_APP_PARSE_ARGS_SUCCESS,
+    spdk_env_fini, spdk_env_init, spdk_env_opts, spdk_env_opts_init, spdk_nvme_ctrlr,
+    spdk_nvme_ctrlr_get_first_active_ns, spdk_nvme_ctrlr_get_next_active_ns,
+    spdk_nvme_ctrlr_get_ns, spdk_nvme_ctrlr_opts, spdk_nvme_ns, spdk_nvme_ns_is_active,
+    spdk_nvme_probe, spdk_nvme_qpair, spdk_nvme_transport_id, spdk_nvme_trid_populate_transport,
+    SPDK_NVME_TRANSPORT_PCIE,
 };
+use std::collections::VecDeque;
+use std::ptr::addr_of_mut;
 
-extern "C" fn hello_world_parse_arg(_ch: c_int, _arg: *mut c_char) -> c_int {
-    0
+#[allow(non_camel_case_types)]
+struct ctrlr_entry {
+    ctrlr: *mut spdk_nvme_ctrlr,
+    // name: [u8; 1024],
 }
 
-extern "C" fn hello_world_usage() {}
+#[allow(non_camel_case_types)]
+struct ns_entry {
+    ctrlr: *mut spdk_nvme_ctrlr,
+    ns: *mut spdk_nvme_ns,
+    qpair: Option<spdk_nvme_qpair>,
+}
+static mut G_CONTROLLERS: VecDeque<ctrlr_entry> = VecDeque::new();
+static mut G_NAMESPACES: VecDeque<ns_entry> = VecDeque::new();
+static mut G_TRID: spdk_nvme_transport_id = unsafe { zeroed() };
 
-extern "C" fn hello_world_start(_arg: *mut c_void) {
-    println!("hello world started!");
+extern "C" fn probe_cb(
+    _cb_ctx: *mut ::std::os::raw::c_void,
+    _trid: *const spdk_nvme_transport_id,
+    _opts: *mut spdk_nvme_ctrlr_opts,
+) -> bool {
+    println!("Attaching to {}", unsafe {
+        CString::from_raw((*_trid).traddr.as_ptr() as *mut c_char)
+            .to_str()
+            .unwrap()
+    });
 
-    std::thread::sleep(std::time::Duration::from_secs(2));
+    true
+}
 
-    println!("hello world ended");
+extern "C" fn attach_cb(
+    _cb_ctx: *mut ::std::os::raw::c_void,
+    _trid: *const spdk_nvme_transport_id,
+    ctrlr: *mut spdk_nvme_ctrlr,
+    _opts: *const spdk_nvme_ctrlr_opts,
+) {
+    println!("Attached to {}", unsafe {
+        CString::from_raw((*_trid).traddr.as_ptr() as *mut c_char)
+            .to_str()
+            .unwrap()
+    });
+
+    let mut ns: *mut spdk_nvme_ns;
+    let entry: ctrlr_entry = ctrlr_entry {
+        ctrlr,
+        // name: [0; 1024],
+    };
+    unsafe {
+        G_CONTROLLERS.push_back(entry);
+    }
+
+    // let cdata: *const spdk_nvme_ctrlr_data = unsafe { spdk_nvme_ctrlr_get_data(ctrlr) };
+    // entry.name = unsafe {
+    //     let name = CString::from_raw((*cdata).mn.as_ptr() as *mut c_char);
+    //     name.into_bytes()
+    // };
 
     unsafe {
-        spdk_app_stop(0);
+        let mut nsid = spdk_nvme_ctrlr_get_first_active_ns(ctrlr);
+        while nsid != 0 {
+            ns = spdk_nvme_ctrlr_get_ns(ctrlr, nsid);
+            if ns.is_null() {
+                continue;
+            }
+
+            if !spdk_nvme_ns_is_active(ns) {
+                nsid = spdk_nvme_ctrlr_get_next_active_ns(ctrlr, nsid);
+                continue;
+            }
+
+            G_NAMESPACES.push_back(ns_entry {
+                ctrlr,
+                ns,
+                qpair: None,
+            });
+            nsid = spdk_nvme_ctrlr_get_next_active_ns(ctrlr, nsid);
+        }
     }
 }
 
@@ -40,39 +103,32 @@ fn main() {
 
         c_args.push(std::ptr::null());
 
+        spdk_nvme_trid_populate_transport(addr_of_mut!(G_TRID), SPDK_NVME_TRANSPORT_PCIE);
+
         // Set default values in opts structure.
-        let mut opts: spdk_app_opts = zeroed();
-        spdk_app_opts_init(
-            &mut opts as *mut _,
-            std::mem::size_of::<spdk_app_opts>() as size_t,
-        );
+        let mut opts: spdk_env_opts = zeroed();
+        spdk_env_opts_init(&mut opts as *mut _);
 
-        opts.name = CString::new("hello_world_test".to_owned())
-            .unwrap()
-            .into_raw();
-
-        // Parse built-in SPDK command line parameters as well
-        // as our custom one(s).
-        let rc = spdk_app_parse_args(
-            (c_args.len() as c_int) - 1,
-            c_args.as_ptr() as *mut *mut c_char,
-            &mut opts as *mut _,
+        let rc = spdk_env_init(&mut opts);
+        println!("spdk_env_init rc: {}", rc);
+        if rc < 0 {
+            panic!("spdk_env_init failed");
+        }
+        let rc = spdk_nvme_probe(
+            addr_of_mut!(G_TRID),
             std::ptr::null_mut(),
-            std::ptr::null_mut(),
-            Some(hello_world_parse_arg),
-            Some(hello_world_usage),
-        );
-        assert_eq!(rc, SPDK_APP_PARSE_ARGS_SUCCESS);
-
-        println!("spdk_app_start()");
-        let rc = spdk_app_start(
-            &mut opts as *mut _,
-            Some(hello_world_start),
-            std::ptr::null_mut(),
+            Some(probe_cb),
+            Some(attach_cb),
+            None,
         );
         assert_eq!(rc, 0);
 
-        println!("spdk_app_fini()");
-        spdk_app_fini();
+        if G_CONTROLLERS.is_empty() {
+            // TODO: need to clear more gracefully
+            panic!("no NVMe controllers found");
+        }
+
+        println!("Found {} controllers", G_CONTROLLERS.len());
+        spdk_env_fini();
     }
 }
